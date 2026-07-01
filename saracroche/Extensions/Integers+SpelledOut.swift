760414da8dfc137e40c8ad796284fc850ae447dd
Camille Bouvat <mail@cbouvat.com>
2026-07-01 17:56:41 +0200

    chore: remove license headers from source files

--- saracroche/Extensions/Integers+SpelledOut.swift
+++ saracroche/Extensions/Integers+SpelledOut.swift
@@ -1,11 +1,3 @@
-// Software Name: Saracroche iOS
-// SPDX-FileCopyrightText: Copyright (c) Camille Bouvat
-// SPDX-License-Identifier: GPL-3.0-or-later
-//
-// This software is distributed under the GNU General Public License v3.0 or later license,
-// the text of which is available at https://www.gnu.org/licenses/gpl-3.0.en.html#license-text
-// or see the "LICENSE" file for more details.
-
 import Foundation
 
 extension Int64 {
